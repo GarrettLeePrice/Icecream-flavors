5c8314610f209a506999ad6e8d57c850f4be918b
Garrett Price <Guest@epicodus-15.local>
2016-08-16 10:21:20 -0700

    changes for.each loop to for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   var flavors = ["vanilla","mint","chocolate","strawberry"];
-  var li_ids = ["#flavor1", "#flavor2", "#flavor3", "#flavor4"];
-  var i = 0;
-    flavors.forEach(function(flavor){
-      $(li_ids[i]).text(flavor);
-      i++;
-    });
+  for (var i=0; i< 4; i+=1) {
+    $("ol").append("<li>" + flavors[i] + "</li>");
+  };
 });
+
+
+  // var li_ids = ["#flavor1", "#flavor2", "#flavor3", "#flavor4"];
+// flavors.forEach(function(flavor){
+//   $(li_ids[i]).text(flavor);
+//   i++;
